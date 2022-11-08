56c36f52afcb148121d60698aefe1ce096996114
Mxolisi <nyawose57@gmail.com>
2022-11-08 09:40:42 +0200

    save changes on routes

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const flash = require('express-flash')
 const session = require('express-session')
 const Waitersfunc = require('./waitersfac')
+const waitersfunc2 = Waitersfunc(db)
 const pgp = require('pg-promise')({});
 
 
@@ -16,7 +17,6 @@ app.set('view engine', 'handlebars');
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-// waitersapp
 
 // create database akhe;
 // create role sibone login password 'sibone123';
@@ -42,153 +42,29 @@ app.use(flash());
 app.use(express.static('public'))
 
 const db = pgp(config);
-const waitersFunction = Waitersfunc(db)
 
-app.get('/', function (req, res) {
-   res.render('index')
-})
+const Routes= require('./routes/routesfunc')
+const Routes= Routes(waitersfunc2)
 
-app.get('/admin', function (req, res) {
-
-   res.render('Admin')
-})
-
-app.post('/admin', function (req, res) {
-   
-   res.redirect('/days')
-})
-
-app.get('/waiter', function (req, res) {
-   res.redirect('/index')
-})
+app.get('/', Routes.index)
 
 
-app.post('/adduser', async function (req, res) {
-   const username = req.body.userInput.charAt().toUpperCase() + req.body.userInput.slice(1).toLowerCase();
-   let alphabet = /^[a-z A-Z]+$/
-   const code = uid();
-
-   let result = await waitersFunction.selectUsername(username)
-  
-   
-if (alphabet.test(username) == false) {
-      req.flash('errormsg', 'Invalid username')
-      res.redirect('/')
-  }
- else if (Number(result.count) !== 0) {
-   res.redirect('/waiters/' + username)
-}
-   else {
-      req.session.code = code
-      await waitersFunction.setUsername(username, code)
-      res.redirect('/login')
-   }
-})
-
-app.get('/login', async function (req, res) {
-   const authcode = req.session.code;
-
-   if(authcode){
-      req.flash('succescode', "Authentication code: " +authcode)
-   }
-   res.render("code")
-
-})
+app.get('/admin', Routes.Admin)
 
 
-app.post('/login', async function (req, res) {
-   let { code } = req.body;
-   let userEntered = await waitersFunction.authCode(code)
+app.post('/admin', Routes.admin)
 
-   if (userEntered) {
-      delete req.session.code;
-      req.session.userEntered = userEntered
-      res.redirect("/waiters/" + userEntered.username)
-      
-    
-   } else {
-      req.flash("errormsg", "invalid details")
-      res.redirect("/login")
-   }
-})
+app.get('/waiter', Routes.waiter)
 
-app.get('/waiters/:username', async function (req, res) {
-   let user = req.params.username.charAt().toUpperCase() + req.params.username.slice(1).toLowerCase();;
-   let output = ` ${user} `;
-   let usernameID= await waitersFunction.usernameID(req.params.username)
-   let result= await waitersFunction.getDays(usernameID)
-   let week= await waitersFunction.weekdays()
-   week = week.map(day => {
+app.post('/adduser', Routes.adduser)
 
-      const checked = result.filter(item => item.workday == day.workday)
-
-      return {
-         ...day,
-         status: checked.length > 0 ? 'checked' : ''
-      }
-   })
-
-   res.render('shift', {
-      user,
-      output,
-      week
-   });
-})
-
-
-app.post('/waiters/:name', async function (req, res) {
-   let workday = req.body.day;
-   let user = req.params.name;
-   
-   if(typeof workday == "string"){
-      req.flash('errormsg', 'Please do not select less than 3 days');
-
-   }else if (workday.length >= 3) {
-      await waitersFunction.setWeek(workday, user);
-      req.flash('success', "scheduled suceesfully")}
-
-      else{
-
-         req.flash('errormsg', 'Please do not select less than 3 days');   
-     }
-
-     res.redirect('back')
-})
-
-app.get('/delete', async function (req, res) {
-   await waitersFunction.removeWaiters()
-   req.flash('succesmsg', "Scheduled cleared sucessfully")
-   res.redirect('/days')
-})
-
-app.get('/days', async function (req, res) {
-
-   let monday = await waitersFunction.JoinTables('Monday')
-   let tuesday = await waitersFunction.JoinTables('Tuesday')
-   let wednesday = await waitersFunction.JoinTables('Wednesday')
-   let thursday = await waitersFunction.JoinTables('Thursday')
-   let friday = await waitersFunction.JoinTables('Friday')
-   let saturday = await waitersFunction.JoinTables('Saturday')
-   let sunday = await waitersFunction.JoinTables('Sunday')
-   let colorChange= await waitersFunction.getColors()
-   res.render('schedule', {
-      monday,
-      tuesday,
-      wednesday,
-      thursday,
-      friday,
-      saturday,
-      sunday,
-      colorChange
-   })
-})
-
-
-app.get('/logout', function (req, res) {
-   delete req.session.user
-   res.redirect('/')
-})
-
+app.get('/login', Routes.login)
+app.post('/login', Routes.login2)
+app.get('/waiters/:username', Routes.selectDay)
+app.post('/waiters/:name', Routes.workDayCheck)
+app.get('/delete', Routes.deleteWaiters)
+app.get('/days', Routes.shedulingDay)
+app.get('/logout', Routes.home)
 // setTimeout(() => {
 //    const box = document.getElementById('succesmsg');
  
