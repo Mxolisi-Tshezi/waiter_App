ea43307fb4ff2a756d1c6c96f44a3e26f0be0702
Mxolisi <nyawose57@gmail.com>
2022-11-08 09:44:13 +0200

    save changes on routes

--- database.sql
+++ database.sql
@@ -3,21 +3,17 @@ create table workers(
     username text not null,
     code varchar
 );
-
 create table workdays(
 	id serial not null primary key,
     workday text not null
 );
-
 create table admins(
     id serial not null primary key,
     user_id integer,
-    -- FOREIGN KEY (user_id) references workers(id),
+    FOREIGN KEY (user_id) references workers(id),
     day_id integer
-    -- FOREIGN KEY (day_id) references workdays(id)
-
+    FOREIGN KEY (day_id) references workdays(id)
 );
-
 INSERT INTO workdays(workday) values ('Monday'), ('Tuesday'), ('Wednesday'), ('Thursday'), ('Friday'), ('Saturday'), ('Sunday');
 
--- INSERT INTO workers(username) values('Mxolisi');
+INSERT INTO workers(username) values('Mxolisi');
