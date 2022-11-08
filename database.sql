3cfab6228f3d146e461a3c03d4bdaacee9736b58
Mxolisi Tshezi <71215268+Mxolisi-Tshezi@users.noreply.github.com>
2022-11-08 17:35:18 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -10,9 +10,7 @@ create table workdays(
 create table admins(
     id serial not null primary key,
     user_id integer,
---     FOREIGN KEY (user_id) references workers(id),
     day_id integer
---     FOREIGN KEY (day_id) references workdays(id)
 );
 INSERT INTO workdays(workday) values ('Monday'), ('Tuesday'), ('Wednesday'), ('Thursday'), ('Friday'), ('Saturday'), ('Sunday');
 
